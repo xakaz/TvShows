892b99514ecbb4edf606f64ad52c2c89266b3a26
xakaz <72222889+xakaz@users.noreply.github.com>
2023-01-20 17:59:09 +0100

    tv shows 1st repo

---
+++ src/App.jsx
@@ -0,0 +1,101 @@
+import { useState, useEffect } from "react";
+import { TVShowAPI } from "./api/tv-show";
+import "./global.css";
+import s from "./style.module.css";
+import { BACKDROP_BASE_URL } from "./config";
+import { TVShowDetail } from "./components/TVShowDetail/TVShowDetail";
+import { Logo } from "./components/Logo/Logo";
+import logo from "./assets/images/logo.png";
+import { TVShowListItem } from "./components/TVShowListItem/TVShowListItem";
+import { TVShowList } from "./components/TVShowList/TVShowList";
+import { SearchBar } from "./components/SearchBar/SearchBar";
+
+export function App() {
+  const [currentTVShow, setCurrentTVShow] = useState();
+
+  const [recommendationList, setRecommendationList] = useState([]);
+
+  async function getPopulars() {
+    try {
+      const populars = await TVShowAPI.fetchPopulars();
+      if (populars.length > 0) {
+        setCurrentTVShow(populars[0]);
+      }
+    } catch (error) {
+      alert("Erreur durant la recherche des séries");
+    }
+  }
+
+  async function getRecommendations(tvShowId) {
+    try {
+      const recommendations = await TVShowAPI.fetchRecommendations(tvShowId);
+      if (recommendations.length > 0) {
+        setRecommendationList(recommendations.slice(0, 10));
+      }
+    } catch (error) {
+      alert("Erreur durant la recherche des séries recommandées");
+    }
+  }
+
+  useEffect(() => {
+    getPopulars();
+  }, []);
+
+  useEffect(() => {
+    if (currentTVShow) {
+      getRecommendations(currentTVShow.id);
+    }
+  }, [currentTVShow]);
+
+  async function searchTVShow(tvShowName) {
+    try {
+      const searchResponse = await TVShowAPI.fetchByTitle(tvShowName);
+      if (searchResponse.length > 0) {
+        setCurrentTVShow(searchResponse[0]);
+      }
+    } catch (error) {
+      alert("Erreur durant la recherche de la série");
+    }
+  }
+
+  return (
+    <div
+      className={s.main_container}
+      style={{
+        background: currentTVShow
+          ? `linear-gradient(rgba(0,0,0,0.55),rgba(0,0,0,0.55)), url("${BACKDROP_BASE_URL}${
+              currentTVShow.backdrop_path === null
+                ? currentTVShow.poster_path
+                : currentTVShow.backdrop_path
+            }") no-repeat center / cover`
+          : "black",
+      }}
+    >
+      <div className={s.header}>
+        <div className="row">
+          <div className="col-4">
+            <Logo
+              title="Watowatch"
+              subtitle="Find a show you may like"
+              image={logo}
+            />
+          </div>
+          <div className="col-sm-12 col-md-8 col-xl-4">
+            <SearchBar onSubmit={searchTVShow} />
+          </div>
+        </div>
+      </div>
+      <div className={s.tv_show_detail}>
+        {currentTVShow && <TVShowDetail tvShow={currentTVShow} />}
+      </div>
+      <div className={s.recommendations}>
+        {recommendationList && recommendationList.length > 0 && (
+          <TVShowList
+            onClickItem={setCurrentTVShow}
+            tvShowList={recommendationList}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ src/config.js
@@ -0,0 +1,7 @@
+const BASE_URL = "https://api.themoviedb.org/3/";
+const API_KEY_PARAM = "?api_key=5a69a81bb489d680e520d3ddc76f7b5d";
+const LANGUAGE = "&language=fr-FR"
+const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original/";
+const SMALL_IMG_COVER_BASE_URL = "https://image.tmdb.org/t/p/w300/";
+
+export { BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL, SMALL_IMG_COVER_BASE_URL, LANGUAGE };
